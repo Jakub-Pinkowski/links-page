352c3abfa28bed5c5e02abc74faf151cf74295b8
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2023-11-11 17:02:25 +0100

    Add functionality to confirm username and update user data

--- src/routes/login/username/+page.svelte
+++ src/routes/login/username/+page.svelte
@@ -26,7 +26,29 @@
         }, 500)
     }
 
-    async function confirmUsername() {}
+    async function confirmUsername() {
+        console.log('confirming username', username)
+        const batch = writeBatch(db)
+        batch.set(doc(db, 'usernames', username), { uid: $user?.uid })
+        batch.set(doc(db, 'users', $user!.uid), {
+            username,
+            photoURL: $user?.photoURL ?? null,
+            published: true,
+            bio: 'I am the Walrus',
+            links: [
+                {
+                    title: 'Test Link',
+                    url: 'https://kung.foo',
+                    icon: 'custom',
+                },
+            ],
+        })
+
+        await batch.commit()
+
+        username = ''
+        isAvailable = false
+    }
 </script>
 
 <AuthCheck>
