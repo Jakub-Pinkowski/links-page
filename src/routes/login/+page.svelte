040762c4fee571bf54e6ca40a976d1a38cada6bc
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2023-11-11 19:28:54 +0100

    Add Google sign-in and sign-out functionality

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -5,8 +5,22 @@
 
     async function signInWithGoogle() {
         const provider = new GoogleAuthProvider()
-        const user = await signInWithPopup(auth, provider)
-        console.log(user)
+        const credential = await signInWithPopup(auth, provider)
+
+        const idToken = await credential.user.getIdToken()
+
+        const res = await fetch('/api/signin', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ idToken }),
+        })
+    }
+
+    async function signOutSSR() {
+        const res = await fetch('/api/signin', { method: 'DELETE' })
+        await signOut(auth)
     }
 </script>
 
