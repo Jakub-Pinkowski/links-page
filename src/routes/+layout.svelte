15b9670376cdd5177e6c63ff2ec417fcd23c7307
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2023-11-11 21:54:48 +0100

    Add footer to layout component

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,5 +3,13 @@
 </script>
 
 <div class="min-h-screen flex flex-col">
-    <slot />
+    <slot class="flex-grow" />
+    <footer class="footer items-center p-4 bg-neutral text-neutral-content mt-auto">
+        <p>© 2023 Jakub Pinkowski</p>
+        <nav class="grid-flow-col gap-4 md:place-self-center md:justify-self-end">
+            <a href="https://github.com/Jakub-Pinkowski">
+                <img class="w-7" src="github_icon.png" alt="github" />
+            </a>
+        </nav>
+    </footer>
 </div>
