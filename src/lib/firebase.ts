6435dfa8f6e0fc1ff1a29502c79e5d671f80602b
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2023-11-11 16:49:41 +0100

    Add user store to firebase module and update login page to display user info and allow sign out

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,7 +1,8 @@
 import { initializeApp } from 'firebase/app'
 import { getFirestore } from 'firebase/firestore'
-import { getAuth } from 'firebase/auth'
+import { getAuth, onAuthStateChanged, type User } from 'firebase/auth'
 import { getStorage } from 'firebase/storage'
+import { writable, type Readable, derived } from 'svelte/store'
 
 const firebaseConfig = {
     apiKey: 'AIzaSyARSNjueh5l-a9W0of42utMvYwcFzaelWc',
@@ -16,3 +17,32 @@ export const app = initializeApp(firebaseConfig)
 export const db = getFirestore()
 export const auth = getAuth()
 export const storage = getStorage()
+
+/**
+ * @returns a store with the current firebase user
+ */
+function userStore() {
+    let unsubscribe: () => void
+
+    if (!auth || !globalThis.window) {
+        console.warn('Auth is not initialized or not in browser')
+        const { subscribe } = writable<User | null>(null)
+        return {
+            subscribe,
+        }
+    }
+
+    const { subscribe } = writable(auth?.currentUser ?? null, (set) => {
+        unsubscribe = onAuthStateChanged(auth, (user) => {
+            set(user)
+        })
+
+        return () => unsubscribe()
+    })
+
+    return {
+        subscribe,
+    }
+}
+
+export const user = userStore()
