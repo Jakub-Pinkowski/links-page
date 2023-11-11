d0a011b3cf82609774ec1ac493bbcb9b8fad958e
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2023-11-11 16:53:56 +0100

    Refactor code for better performance and readability.

---
+++ src/lib/components/AuthCheck.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    import { user } from '$lib/firebase'
+</script>
+
+{#if $user}
+    <slot />
+{:else}
+    <p class="text-center text-danger">
+        You are not logged in
+        <a class="btn btn-primary" href="login">Sign in</a>
+    </p>
+{/if}
