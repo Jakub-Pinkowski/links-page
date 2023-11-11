723f3de9c54d3e71dd46a19a2cd9c886c049f87d
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2023-11-11 17:08:05 +0100

    Add username validation and availability check to login page

--- src/routes/login/username/+page.svelte
+++ src/routes/login/username/+page.svelte
@@ -9,6 +9,12 @@
 
     let debounceTimer: NodeJS.Timeout
 
+    const re = /^(?=[a-zA-Z0-9._]{3,16}$)(?!.*[_.]{2})[^_.].*[^_.]$/
+
+    $: isValid = username?.length > 2 && username.length < 16 && re.test(username)
+    $: isTouched = username.length > 0
+    $: isTaken = isValid && !isAvailable && !loading
+
     async function checkAvailability() {
         isAvailable = false
         clearTimeout(debounceTimer)
@@ -52,7 +58,6 @@
 </script>
 
 <AuthCheck>
-    <h2>Username</h2>
     <form class="w-2/5" on:submit|preventDefault={confirmUsername}>
         <input
             type="text"
@@ -60,10 +65,28 @@
             class="input w-full"
             bind:value={username}
             on:input={checkAvailability}
+            class:input-error={!isValid && isTouched}
+            class:input-warning={isTaken}
+            class:input-success={isAvailable && isValid && !loading}
         />
+        <div class="my-4 min-h-16 px-8 w-full">
+            {#if loading}
+                <p class="text-secondary">Checking availability of @{username}...</p>
+            {/if}
 
-        <p>Is available? {isAvailable}</p>
+            {#if !isValid && isTouched}
+                <p class="text-error text-sm">must be 3-16 characters long, alphanumeric only</p>
+            {/if}
 
-        <button class="btn btn-success">Confirm username @{username} </button>
+            {#if isValid && !isAvailable && !loading}
+                <p class="text-warning text-sm">
+                    @{username} is not available
+                </p>
+            {/if}
+
+            {#if isAvailable}
+                <button class="btn btn-success">Confirm username @{username} </button>
+            {/if}
+        </div>
     </form>
 </AuthCheck>
